13053ef93f8719fa62a37151802c1a19d4c60236
s4rv4d <sarvadshetty@gmail.com>
2018-05-16 18:57:52 -0500

    Profile layout done and hooked up with firebase added dummy data

---
+++ Internals/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  Internals
+//
+//  Created by Sarvad shetty on 5/16/18.
+//  Copyright © 2018 Sarvad shetty. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    //MARK:IBOutlets
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var cellOption: UIButton!
+    @IBOutlet weak var taskLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
